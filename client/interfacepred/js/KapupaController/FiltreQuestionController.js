240534ec2ac0b3e0920b9c3702e7c7590678a884
Majda Salim <Majda Salim>
2017-02-17 16:27:48 +0100

    version avant finale

--- client/interfacepred/js/KapupaController/FiltreQuestionController.js
+++ client/interfacepred/js/KapupaController/FiltreQuestionController.js
@@ -3,8 +3,8 @@ app.controller('SelectQuestionController',function($scope,$http){
   var ques;
   var clientid;
 
-  $("#questions").change(function(){
 
+  $("#questions").change(function(){
 
     clientid= $("#champcaché").val();
     ques= $("#questions").val();
@@ -14,64 +14,70 @@ app.controller('SelectQuestionController',function($scope,$http){
 
       $.each( response, function( key, value ) {
         if( value.clientId==clientid && value.title==ques){
-            $("#retourclient").text(value.feedBack.hasOwnProperty('realNb') ? value.feedBack.realNb : "non renseigné");
-             $("#estimation").text(value.hasOwnProperty('estimation') ? value.estimation : "non renseigné");
-             alert('title : ' + value.title);
-             console.log(value.feedBack);
-             
-             $("#tauxerreur").text(value.hasOwnProperty('successRate') ? value.successRate : "non renseigné");
-     
-            google.charts.load('current', {'packages':['bar','corechart']});
-            google.charts.setOnLoadCallback(drawStuff);
 
-            function drawStuff() {
-              var data = new google.visualization.arrayToDataTable([
-                ['', ''],
-                ["envoyés", parseInt(value.nbPull.mobilePull)+parseInt(value.nbPull.websitePull)],
-                ["répondus",  parseInt(value.answer.stat.total)]
+            $("#estimation").text(value.hasOwnProperty('estimation') ? value.estimation : "non renseigné");
+            $("#tauxerreur").text(value.hasOwnProperty('successRate') ? value.successRate : "non renseigné");
+            $("#retourclient").text(value.hasOwnProperty('feedBack') ? value.feedBack.realNb : "non renseigné");
 
-                ]);
+            if(value.hasOwnProperty('answer'))
+            {
+                google.charts.load('current', {'packages':['bar','corechart']});
+                google.charts.setOnLoadCallback(drawStuff);
 
-              var options = {
-               colors: ['#800040'],
-               width:400,
-               legend: { position: 'none' },
-          bars: 'vertical', // Required for Material Bar Charts.
-          axes: {
-            x: {
+                function drawStuff() {
+                  var data = new google.visualization.arrayToDataTable([
+                    ['', ''],
+                    ["envoyés", parseInt(value.nbPull.mobilePull)+parseInt(value.nbPull.websitePull)],
+                    ["répondus",  parseInt(value.answer.stat.total)]
+
+                    ]);
+
+                  var options = {
+                   colors: ['#800040'],
+                   width:400,
+                   legend: { position: 'none' },
+                   bars: 'vertical', // Required for Material Bar Charts.
+                   axes: {
+                    x: {
               0: { side: 'top', label: 'Nombre de messages'} // Top x-axis.
-            }
-          },
-          bar: { groupWidth: "90%" }
-        };
+          }
+      },
+      bar: { groupWidth: "90%" }
+  };
 
-        var chart = new google.charts.Bar(document.getElementById('top_x_div'));
-        chart.draw(data, options);
-      };
-
-
-
-      google.charts.setOnLoadCallback(drawChart);
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['type de réponses', 'nombre de réponses'],
-          ['Oui',      parseInt(value.answer.stat.positive)],
-          ['Non',     parseInt( value.answer.stat.negative)]
+  var chart = new google.charts.Bar(document.getElementById('top_x_div'));
+  chart.draw(data, options);
+};
 
 
-          ]);
 
-        var options = {
-          colors: ['#FFA500', '#800040'],
-          title: 'Nombre de réponses'
-        };
+google.charts.setOnLoadCallback(drawChart);
+function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+      ['type de réponses', 'nombre de réponses'],
+      ['Oui',      parseInt(value.answer.stat.positive)],
+      ['Non',     parseInt( value.answer.stat.negative)]
 
-        var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
-        chart.draw(data, options);
-      }
+
+      ]);
+
+    var options = {
+      colors: ['#FFA500', '#800040'],
+      title: 'Nombre de réponses'
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+  chart.draw(data, options);
+}
+
+}else {
+ 
+    $("#graphe1").text("non renseigné");
+    $("#graphe2").text("non renseigné");
+}
 }
 });
-})
-$('#divquestion').show();
+  })
+    $('#divquestion').show();
 })
 });
